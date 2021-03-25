de1d517be3de7962f50f6cea84c192df2b553d2d
Victoria Khusti <viktorijahusti@gmail.com>
2021-03-25 13:48:51 +0200

    add method that displays content of repository

--- DataModelingConsoleApplication/Program.cs
+++ DataModelingConsoleApplication/Program.cs
@@ -7,9 +7,47 @@ namespace DataModelingConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var dm = new DataModelingLibrary.DataService();
-            var hello = dm.hello();
-            Console.WriteLine(hello);
+            DisplayContent();
         }
+
+        static void DisplayContent()
+        {
+            var dm = new DataModelingLibrary.DataService();
+
+            string owner = "CSSEGISandData";
+            string repo = "COVID-19";
+            string path = "csse_covid_19_data";
+
+            var contents = dm.listAsync(owner, repo, path).Result;
+            var count = 1;
+            foreach (var file in contents)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                var fileType = (string)file["type"];
+                if (fileType == "dir")
+                {
+                    var directoryContentsUrl = (string)file["url"];
+                    // use this URL to list the contents of the folder
+                    //count++;
+                    Console.WriteLine($" #{count} | DIR: {directoryContentsUrl}");
+                    Console.WriteLine("");
+                    
+                }
+                else if (fileType == "file")
+                {
+                    var downloadUrl = (string)file["download_url"];
+                    // use this URL to download the contents of the file
+                    //count++;
+                    
+                    Console.WriteLine($" #{count} | DOWNLOAD: {downloadUrl}");
+                    Console.WriteLine("");
+                }
+                count++;
+            }
+            Console.ResetColor();
+
+        }
+
+
     }
 }
--- DataModelingLibrary/DataService.cs
+++ DataModelingLibrary/DataService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace DataModelingLibrary
@@ -10,5 +14,29 @@ namespace DataModelingLibrary
             var hello = "hello from lib";
             return hello;
          }
+
+        /// <summary>
+        /// Returns the contents of a GitRepo and displays the URL to download each file 
+        /// </summary>
+        /// <remarks>
+        /// Takes string values like: 
+        /// <c>string owner = "CSSEGISandData", repo = "CSSEGISandData", path = "csse_covid_19_data";</c>
+        /// </remarks>
+
+        public async System.Threading.Tasks.Task<JArray> listAsync(string owner, string repo, string path = "")
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.Add(
+                new ProductInfoHeaderValue("MyApplication", "1"));
+            //var repo = "markheath/azure-deploy-manage-containers";
+            //var repo = "CSSEGISandData/COVID-19";
+            //var path = "csse_covid_19_data/csse_covid_19_daily_reports";
+            var contentsUrl = $"https://api.github.com/repos/{owner}/{repo}/contents/{path}";
+           // var contentsUrl = $"https://api.github.com/repos/{repo}/git/trees/master";
+            var contentsJson = await httpClient.GetStringAsync(contentsUrl);
+            var contents = (JArray)JsonConvert.DeserializeObject(contentsJson);
+
+            return contents;
+        } 
     }
 }
