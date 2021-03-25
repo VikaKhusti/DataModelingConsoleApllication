070319bd5bdac5dbb480e8040138ec6e3d808fa7
Victoria Khusti <viktorijahusti@gmail.com>
2021-03-25 11:28:20 +0200

    initial commit

---
+++ DataModelingConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataModelingConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var dm = new DataModelingLibrary.DataService();
+            var hello = dm.hello();
+            Console.WriteLine(hello);
+        }
+    }
+}
---
+++ DataModelingLibrary/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModelingLibrary
+{
+    public class DataService
+    {
+         public string hello() {
+            var hello = "hello from lib";
+            return hello;
+         }
+    }
+}
