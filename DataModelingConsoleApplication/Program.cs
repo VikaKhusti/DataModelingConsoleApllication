d30e29c7b25ede4f9021336b40c0920fe0b79032
Victoria Khusti <viktorijahusti@gmail.com>
2021-03-26 18:02:56 +0200

    work with data display

--- DataModelingConsoleApplication/Program.cs
+++ DataModelingConsoleApplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DataModelingConsoleApplication
 {
@@ -6,46 +7,96 @@ namespace DataModelingConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            DisplayContent();
-        }
+            
 
-        static void DisplayContent()
-        {
-            var dm = new DataModelingLibrary.DataService();
-
+            //csse_covid_19_data/csse_covid_19_daily_reports
             string owner = "CSSEGISandData";
             string repo = "COVID-19";
-            string path = "csse_covid_19_data";
+            string path = "csse_covid_19_data/";
+            DisplayContent(owner, repo, path);
+        }
 
-            var contents = dm.listAsync(owner, repo, path).Result;
+        static void DisplayContent(string owner, string repo, string path)
+        {
+            var dm = new DataModelingLibrary.DataService();
             var count = 1;
-            foreach (var file in contents)
+            Dictionary<int, string> urls = new Dictionary<int, string>();
+            try
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                var fileType = (string)file["type"];
-                if (fileType == "dir")
+                var contents = dm.listAsync(owner, repo, path).Result;
+                foreach (var file in contents)
                 {
-                    var directoryContentsUrl = (string)file["url"];
-                    // use this URL to list the contents of the folder
-                    //count++;
-                    Console.WriteLine($" #{count} | DIR: {directoryContentsUrl}");
-                    Console.WriteLine("");
-                    
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    var fileType = (string)file["type"];
+                    if (fileType == "dir")
+                    {
+                        var directoryContentsUrl = (string)file["url"];
+                        urls.Add(count, directoryContentsUrl);
+                        Console.WriteLine($" #{count} | DIR: {directoryContentsUrl}");
+                        Console.WriteLine("");
+
+                    }
+                    else if (fileType == "file")
+                    {
+                        var downloadUrl = (string)file["download_url"];
+                        urls.Add(count, downloadUrl);
+                        Console.WriteLine($" #{count} | DOWNLOAD: {downloadUrl}");
+                        Console.WriteLine("");
+                    }
+                    count++;
                 }
-                else if (fileType == "file")
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Type 0 to exit or '..' to go back");
+                Console.WriteLine("Type # to open item (0 to exit)");
+                
+                try
                 {
-                    var downloadUrl = (string)file["download_url"];
-                    // use this URL to download the contents of the file
-                    //count++;
-                    
-                    Console.WriteLine($" #{count} | DOWNLOAD: {downloadUrl}");
-                    Console.WriteLine("");
-                }
-                count++;
+                    var input = Console.ReadLine().Trim();
+                    if (input.ToUpper() == "EXIT") return;
+                    else
+                    {
+                        var item = urls[Convert.ToInt32(input)];
+                        Console.WriteLine(item);
+                        Console.WriteLine(SplitUrl(item));
+                        String[] arr = SplitUrl(item);
+                        foreach (var i in arr)
+                        {
+                            Console.WriteLine(i);
+                        }
+
+                    }
+
+                } catch (Exception e)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Invalid # entered. Try again? y/n");
+                    var answer = Console.ReadLine().Trim();
+                    try
+                    {
+                        if (answer == "n") return;
+                    }
+                    catch (Exception e1)
+                    {
+                        Console.WriteLine(e1);
+                    }
+                }                
+
+            } catch (AggregateException ae)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Enable to display data");
             }
+            
             Console.ResetColor();
 
+        }
+
+        public static String[] SplitUrl(string url)
+        {
+            String[] point = url.Split('/');
+            return point;
         }
 
 
